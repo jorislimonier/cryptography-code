1e2f135326a4f19fc2c0a5cb855350bb131e1f0d
jorislimonier <joris.limonier@hotmail.fr>
2021-05-22 21:08:26 +0200

    use powermod to prevent overflow

--- 05_rsa/rsa.jl
+++ 05_rsa/rsa.jl
@@ -1,33 +1,38 @@
 using Primes
 include("./utils/utils.jl")
 
+function φ(modulus)
+    return totient(modulus)
+end
+
 p = 29
 q = 53
-modulus = p * q
+public_exp = 3 # e
 
-public_exp = 3
-private_exp = 971
-public_key = (1537, 3)
+modulus = p * q # n
+private_exp = invmod(public_exp, φ(modulus))
+public_key = (modulus, public_exp)
 private_key = (modulus, private_exp)
 
-phi = (p - 1) * (q - 1)
+message = 101 # m
 
-message = 101
 
-function encrypt()
+
+function encrypt(message)
     return message^public_exp % modulus
 end
 
-c = encrypt()
+c = encrypt(message)
 
-function decrypt()
-    return (c^private_exp) % modulus
+function decrypt(c)
+    return powermod(c, private_exp, modulus)
 end
+decrypt(c)
 
-decrypt()
 
+isprime(1456)
 a = 241
 b = 46
 
-s0, t0 = Utils.eucl_ext_alg(a, b)
+s0, t0 = Utils.bez_coef(a, b)
 s0 * a + t0 * b
